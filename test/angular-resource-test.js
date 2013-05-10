209340a78bff8f274f01765084bb3d4250d09df6
Aaron <aaron@aaron-murray.net>
2013-05-10 16:56:05 -0700

    Changed to using object.save() instead of object.create() for POSTs to /endpoint

--- test/angular-resource-test.js
+++ test/angular-resource-test.js
@@ -1,60 +1,60 @@
 var assert = require('assert'),
-		angularResource = require('../lib/angular-resource'),
-		sinon = require('sinon'),
-		testObject = require('./testObject');
+  angularResource = require('../lib/angular-resource'),
+  sinon = require('sinon'),
+  testObject = require('./testObject');
 
-describe('angular-resource', function() {
-	var app;
-	beforeEach(function() {
-		app = {
-			"get": sinon.stub(),
-			"post": sinon.stub(),
-			"delete": sinon.stub()
-		};
-	});
-	describe('without middleware', function() {
-		describe('using testObject', function() {
-			beforeEach(function() {
-				angularResource(app, '/api', 'testObject');
-			});
-			it('should bind get', function() {
-				assert(app.get.calledWith('/api/testObject/:id', testObject.get));
-			});
-			it('should bind save', function() {
-				assert(app.post.calledWith('/api/testObject/:id', testObject.save));
-			});
-      it('should bind create', function() {
-          assert(app.post.calledWith('/api/testObject', testObject.create));
+describe('angular-resource', function () {
+  var app;
+  beforeEach(function () {
+    app = {
+      "get": sinon.stub(),
+      "post": sinon.stub(),
+      "delete": sinon.stub()
+    };
+  });
+  describe('without middleware', function () {
+    describe('using testObject', function () {
+      beforeEach(function () {
+        angularResource(app, '/api', 'testObject');
       });
-			it('should bind query', function() {
-				assert(app.get.calledWith('/api/testObject', testObject.query));
-			});
-			it('should bind remove', function() {
-				assert(app["delete"].calledWith('/api/testObject/:id', testObject.remove));
-			});
-		});
-	});
-	describe('with middleware', function() {
-		var middleware = { };
-		describe('using testObject', function() {
-			beforeEach(function() {
-				angularResource(app, '/api', 'testObject', middleware);
-			});
-			it('should bind get', function() {
-				assert(app.get.calledWith('/api/testObject/:id', middleware, testObject.get));
-			});
-			it('should bind save', function() {
-				assert(app.post.calledWith('/api/testObject/:id', middleware, testObject.save));
-			});
-      it('should bind create', function() {
-          assert(app.post.calledWith('/api/testObject', middleware, testObject.create));
+      it('should bind get', function () {
+        assert(app.get.calledWith('/api/testObject/:id', testObject.get));
       });
-			it('should bind query', function() {
-				assert(app.get.calledWith('/api/testObject', middleware, testObject.query));
-			});
-			it('should bind remove', function() {
-				assert(app["delete"].calledWith('/api/testObject/:id', middleware, testObject.remove));
-			});
-		});
-	});
+      it('should bind save', function () {
+        assert(app.post.calledWith('/api/testObject/:id', testObject.save));
+      });
+      it('should bind create', function () {
+        assert(app.post.calledWith('/api/testObject', testObject.save));
+      });
+      it('should bind query', function () {
+        assert(app.get.calledWith('/api/testObject', testObject.query));
+      });
+      it('should bind remove', function () {
+        assert(app["delete"].calledWith('/api/testObject/:id', testObject.remove));
+      });
+    });
+  });
+  describe('with middleware', function () {
+    var middleware = { };
+    describe('using testObject', function () {
+      beforeEach(function () {
+        angularResource(app, '/api', 'testObject', middleware);
+      });
+      it('should bind get', function () {
+        assert(app.get.calledWith('/api/testObject/:id', middleware, testObject.get));
+      });
+      it('should bind save', function () {
+        assert(app.post.calledWith('/api/testObject/:id', middleware, testObject.save));
+      });
+      it('should bind create', function () {
+        assert(app.post.calledWith('/api/testObject', middleware, testObject.save));
+      });
+      it('should bind query', function () {
+        assert(app.get.calledWith('/api/testObject', middleware, testObject.query));
+      });
+      it('should bind remove', function () {
+        assert(app["delete"].calledWith('/api/testObject/:id', middleware, testObject.remove));
+      });
+    });
+  });
 });
--- test/testObject.js
+++ test/testObject.js
@@ -1,18 +1,15 @@
 var testObject = { };
 
-testObject.get = function() {
+testObject.get = function () {
 };
 
-testObject.save = function() {
+testObject.save = function () {
 };
 
-testObject.create = function() {
+testObject.query = function () {
 };
 
-testObject.query = function() {
-};
-
-testObject.remove = function() {
+testObject.remove = function () {
 };
 
 module.exports = testObject;
